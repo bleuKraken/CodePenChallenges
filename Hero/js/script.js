cbb8bf71d11771b25f083665f6e6d2c76a2878ba
erickkg <erick31641@yahoo.com>
2020-04-12 18:29:00 -0700

    Finish JS code to generate text. cleaned html and css

--- Hero/js/script.js
+++ Hero/js/script.js
@@ -1,10 +1,12 @@
 const generateButton = document.getElementById('generate-quote');
 
+// Event listener
 generateButton.addEventListener("click", function(){
-  console.log("Ow, clicking hurts me!");
+  newQuote();
 });
 
-let quotes = {
+//quotes
+let quotes = [
   "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.",
   "Anyone who has never made a mistake has never tried anything new.",
   "The world is a dangerous place to live; not because of the people who are evil, but because of the people who don't do anything about it.",
@@ -14,4 +16,9 @@ let quotes = {
   "Education is what remains after one has forgotten what one has learned in school.",
   "Only one who devotes himself to a cause with his whole strength and soul can be a true master. For this reason mastery demands all of a person.",
   "Logic will get you from A to B. Imagination will take you everywhere."
+]
+
+function newQuote() {
+  let randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById("quote").innerHTML = quotes[randomNumber];
 }
